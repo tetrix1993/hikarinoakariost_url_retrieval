f751b35dc1a239c4cfe6e229929f8db82e3cf433
tetrix1993 <tetrix1993@hotmail.com>
2019-11-27 13:31:13 +0800

    Initializing

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# hikarinoakariost_url_retrieval
+# Hikarinoakariost URL Retrieval
+
+## Introduction
+Hikarinoakariost URL Retrieval is a simple command line program to read the [website](https://hikarinoakariost.info/), written specifically to bypass the 3-seconds wait to access the download link. The program is written in Python 3.
+
+## Setting Up
+1. Download and install the latest version of Python (3.7.4).
+2. Open the Command Prompt (for Windows) or Terminal (for MacOS).
+3. Run the following command: `pip install requests`
+
+## Running the Program
+1. Using the Command Prompt, change to the directory to where the file `hikarinoakariost_url_retriever.py` is located. E.g. `cd D:\hikarinoakariost_url_retrieval`
+2. Run the following command: `python hikarinoakariost_url_retriever.py`
+3. The program will load the first page of the the website.
+![image001.png](/images/img001.png)
+4. Select a specific entry you want to download by entering the choice.
+5. After you entered your choice, it will print the download link to the screen.
+![image002.png](/image/img002.png)
+6. If you want to access the other pages of the website, enter 0.
+7. The program will prompt you whether to go the next page or not.
+![image003.png](/image/img003.png)
+8. If you enter 'Y', it will load the next page.
+9. If you enter other values, the program will exit.
---
+++ hikarinoakariost_url_retriever.py
@@ -0,0 +1,135 @@
+import urllib.request
+#import argparse
+import math
+from myutil.util import create_directory
+from myutil.util import download_image
+from myutil.util import get_response
+from myutil.util import get_response_with_header
+from myutil.util import is_file_exists
+
+#parser = argparse.ArgumentParser()
+#parser.add_argument('ID', help = 'ID')
+#parser.add_argument('P', type = int, help = 'number of processes')
+#parser.add_argument('L', type = int, help = 'last page number')
+#args = parser.parse_args()
+
+MAIN_PAGE_PREFIX = "https://hikarinoakariost.info/page/"
+MAIN_PAGE_SUFFIX = "/"
+OUT_LINK_PREFIX = "https://hikarinoakariost.info/out/?"
+NOOPENER_NOREFERRER = 'rel="noopener noreferrer">'
+
+def get_choice():
+    while True:
+        print("Enter your choice. Enter '0' if you are not selecting any of the choices")
+        try:
+            choice = int(input("Enter choice: "))
+            break
+        except Exception as e:
+            print('Please enter a number')
+            pass
+    return choice
+    
+def process_song_page(choice, text_block):
+    text = text_block[choice]
+    split1 = text.split('<a href="')
+    if len(split1) < 2:
+        return
+    page_url = split1[1].split('"')[0]
+    response = get_response_with_header(page_url)
+    split2 = response.split(OUT_LINK_PREFIX)
+    if len(split2) < 2:
+        return
+    #num_choices = 0
+    for i in range(1, len(split2), 1):
+        split3 = split2[i].split('</td>')[0]
+        if NOOPENER_NOREFERRER in split3:
+            split4 = split3.split(NOOPENER_NOREFERRER)
+            if len(split4) < 2:
+                continue
+            host = split4[1].split('</a>')[0]
+            code = split3.split('"')[0]
+            out_link = base64_decode(code)
+            print(str(i) + " - " + host + " " + out_link)
+            #num_choices = num_choices + 1
+    
+def base64_decode(data):
+    b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+    o1 = o2 = o3 = o4 = h1 = h2 = h3 = h4 = bits = i = 0
+    ac = 0
+    dec = ""
+    tmp_arr = []
+    
+    if not data:
+        return data
+    
+    while i < len(data):
+        h1 = b64.index(data[i])
+        i = i + 1
+        h2 = b64.index(data[i])
+        i = i + 1
+        h3 = b64.index(data[i])
+        i = i + 1
+        h4 = b64.index(data[i])
+        i = i + 1
+        
+        bits = h1 << 18 | h2 << 12 | h3 << 6 | h4
+        
+        o1 = bits >> 16 & 0xff
+        o2 = bits >> 8 & 0xff
+        o3 = bits & 0xff
+        
+        tmp_arr.append("")
+        
+        if (h3 == 64):
+            tmp_arr[ac] = chr(o1)
+            ac = ac + 1
+        elif (h4 == 64):
+            tmp_arr[ac] = chr(o1) + chr(o2)
+            ac = ac + 1
+        else:
+            tmp_arr[ac] = chr(o1) + chr(o2) + chr(o3)
+            ac = ac + 1
+        
+    dec = ''.join(tmp_arr)
+    return dec
+
+def get_title(choice, split2):
+    title_temp = split2[choice].split('title="')
+    if len(title_temp) < 2:
+        return ""
+    return title_temp[1].split('">')[0]
+
+def run():
+    #print(base64_decode('aHR0cHM6Ly9vdW8uaW8vR0txNDhM'))
+    page = 1
+    choice = 1
+    goto_next_page = True
+    while (goto_next_page):
+        print('\n>>> Loading page ' + str(page) + ' <<<\n')
+        main_page_url = MAIN_PAGE_PREFIX + str(page) + MAIN_PAGE_SUFFIX
+        response = get_response_with_header(main_page_url)
+        split1 = response.split('<div class="td-container td-pb-article-list ">')
+        if len(split1) < 2:
+            return
+        split2 = split1[1].split('<div class="td-module-thumb">')
+        num_of_choices = len(split2) - 1
+        for i in range(1, len(split2), 1):
+            title = get_title(i, split2)
+            if len(title) == 0:
+                continue
+            print(str(i) + " - " + title)
+        while True:
+            choice = get_choice()
+            if (choice >= 1 and choice <= num_of_choices):
+                title = get_title(choice, split2)
+                print(title)
+                process_song_page(choice, split2)
+            else:
+                break
+        go_next_page = input('Go to next page? Y/N: ')
+        if (not go_next_page in "Y") and (not go_next_page in "y"):
+            goto_next_page = False
+        page = page + 1
+
+if __name__ == '__main__':
+    run()
